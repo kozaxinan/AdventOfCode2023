6aa2093532044946c23f81d9216aa1be2221123a
Sinan Kozak <kozaxinan@gmail.com>
2023-12-03 14:58:44 +0100

    solution for day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,10 +1,53 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map { line ->
+                var first = 0
+                var second = 0
+                line.onEach {
+                    if (it.isDigit()) {
+                        if (first == 0) {
+                            first = it.digitToInt()
+                            second = first
+                        }
+                        if (first != 0) second = it.digitToInt()
+                    }
+                }
+                first * 10 + second
+            }
+            .sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return inputString
+            .map { it.replace("nine", "n9e") }
+            .map { it.replace("eight", "e8t") }
+            .map { it.replace("seven", "7n") }
+            .map { it.replace("six", "6") }
+            .map { it.replace("five", "5e") }
+            .map { it.replace("four", "4") }
+            .map { it.replace("three", "t3e") }
+            .map { it.replace("two", "t2o") }
+            .map { it.replace("one", "o1e") }
+            .map { it.replace("zero", "0o") }
+            .map { line ->
+
+                var first = 0
+                var second = 0
+                line.onEach {
+                    if (it.isDigit()) {
+                        if (first == 0) {
+                            first = it.digitToInt()
+                            second = first
+                        }
+                        if (first != 0) second = it.digitToInt()
+                    }
+                }
+                val i = first * 10 + second
+
+                println("$line $i")
+                i
+            }
     }
 
     // test if implementation meets criteria from the description, like:
